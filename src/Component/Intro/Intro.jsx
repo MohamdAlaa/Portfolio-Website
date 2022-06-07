0a838d1b45e95653f6a6bfa181872f15071b4835
Mohamed Alaa <mohamedelmenyawe606@gmail.com>
2022-06-07 11:26:24 +0200

    finished intro component

--- src/Component/Intro/Intro.jsx
+++ src/Component/Intro/Intro.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import "./Intro.css";
+import Me from "../../Img/Me.jpg"
+
 
 const Intro = () => {
   return (
-    <div>this is intro</div>
-  )
-}
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is</h2>
+          <h1>Mohamed Alaa</h1>
+          <div className="i-tittle">
+            <div className="i-tittle-wrapper">
+              <div className="i-tittle-item">Front End Developer</div>
+            </div>
+          </div>
+          <p className="i-desc">
+            I design and develop services for customers of all sizes,
+            specializing in creating stylish, modern websites, web services and
+            online stores. <br/>Talented and CreativeFront End Developer skilled at designing 
+            sitesand writing clean code. Work Experience Matched content tocustomer specifications
+            through skilled design, writing and editing.<br/>Created innovative designs with use of programming
+            languages,Framework and tools Developed and deployed successfull
+          </p>
 
-export default Intro
+        </div>
+      </div>
+      <div className="i-right">
+        <img src={Me} alt="" className="i-img" />
+      </div>
+
+    </div>
+  );
+};
+
+export default Intro;
